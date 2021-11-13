7fe5dd0b020a2cf5f102790cd00567e34994728e
alexandrecpedro <alexandrecpedro@gmail.com>
2021-11-13 17:17:10 -0300

    Post Cards

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
 window.onload = () => {
+    
     let submitBtn = document.querySelector('button[type=submit]');
     let url = document.querySelector('input[name=imgUrl]');
     let localName = document.querySelector('input[name=titulo]');
     let radioBtns = document.querySelectorAll('input[name=pais]');
     let descricao = document.querySelectorAll('input[name=descricao]');
 
-    // criando card
-    let cartao = document.createElement('div');
-    cartao.className = 'card';
-    let main = document.querySelector('main');
-    main.appendChild(cartao);
-
     submitBtn.addEventListener('click', event => {
         event.preventDefault();
+
+        // criando card
+        let cartao = document.createElement('div');
+        cartao.className = 'card';
 
         // Imagem inserido no card
         let localImagem = document.createElement('div');
@@ -24,23 +23,22 @@ window.onload = () => {
 
         // Titulo do local no card
         let nomeLocal = document.createElement('h3');
-        nomeLocal.innerHTML = `<h3>${localName.value}</h3>`
+        nomeLocal.innerHTML += `<h3>${localName.value}</h3>`
         cartao.appendChild(nomeLocal);
 
         // Verificar qual radioButton foi escolhido
         let escolhido = '';
-        radioBtns.map(selecionado => {
+        radioBtns.forEach(selecionado => {
             (selecionado.checked) ? escolhido = selecionado: ''
         });
 
-        // Região inserida no card
-        let regiao = document.createElement('h4');
-        regiao.innerHTML = `<h4>Região: ${radioBtns[escolhido.value]}</h4>`
-        cartao.appendChild(regiao);
+        cartao.innerHTML += `<h3>${radioBtns[escolhido.value]}</h3>`
 
         // Descrição 
         let description = document.createElement('p');
         description.innerHTML = `<p>${descricao.value}</p>`;
         cartao.appendChild(description);
-    };
+        
+        document.querySelector('#princ').appendChild(cartao)
+    })
 }
